69628f39c95fb691a5971fc16d5510d276c8a088
mohshaik <mohshaik@din56001342.corp.capgemini.com>
2018-04-11 17:02:27 +0530

    Done lab-2

---
+++ Lab-2/src/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee 
+{public static void
+	 main(String [] args)
+	{
+	    Date joiningDate = new Date(24, 10, 1997);
+		Employee1 e1 = new Employee1("ali", "smd",45464, "ada3",joiningDate); 
+		Employee1 e2 = new Employee1("preethi", "R",2342378, "a4",joiningDate); 
+		e1.display();
+		
+	    System.out.println("");
+		e2.display();
+		}
+}
